f395cfd7208a5d93ea9d1d96b759d2c54ddaa8d7
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-07-03 20:13:21 +0100

    update statistics

--- output/stat.js
+++ output/stat.js
@@ -1,4 +1,4 @@
-var txt = '{"release":["2019 Q4"],"price_uk":[-0.6],"price_london":[-3.26],"afford_uk":[-0.23],"afford_london":[-3.09]}';
+var txt = '{"release":["2020 Q1"],"price_uk":[0.73],"price_london":[-0.71],"afford_uk":[0.43],"afford_london":[-0.15]}';
 var obj = JSON.parse(txt);
 document.getElementById("js-release").innerHTML = "Release: <br>" + obj.release;
 document.getElementById("js-price-uk").innerHTML = obj.price_uk + " %";
