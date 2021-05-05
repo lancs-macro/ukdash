edde5f9800d0f8e40bf0041f1110cee095bfa7bf
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2021-05-05 16:27:33 +0100

    update stats

--- output/stat.js
+++ output/stat.js
@@ -1,4 +1,4 @@
-var txt = '{"release":["2020 Q3"],"price_uk":[2.6],"price_london":[3.46],"afford_uk":[3.01],"afford_london":[3.58]}';
+var txt = '{"release":["2021 Q1"],"price_uk":[5.15],"price_london":[3.79],"afford_uk":[5.58],"afford_london":[3.89]}';
 var obj = JSON.parse(txt);
 document.getElementById("js-release").innerHTML = "Release: <br>" + obj.release;
 document.getElementById("js-price-uk").innerHTML = obj.price_uk + " %";
